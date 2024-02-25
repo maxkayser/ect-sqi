2cfac56ce1e2d7ab9dff35b01bd5776a26f4210a
Max Kayser <max.kayser@uni-bonn.de>
2024-02-25 17:33:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ECT-SQI'
+copyright = '2024, Kayser'
+author = 'Kayser'
 
 release = '0.1'
 version = '0.1.0'
